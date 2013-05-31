b325f994cd83ea5c86746ca433253c80c8e8bb8b
Brian Craig <bcraig108@gmail.com>
2013-05-31 17:08:33 -0400

    Cleaned up comments

--- Examples/src/HelloWorld.java
+++ Examples/src/HelloWorld.java
@@ -1,11 +1,6 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
- *
- * @author Brian & Denise
+ * HelloWorld - Lesson 1
+ * @author Brian
  */
 public class HelloWorld {
     public static void main( String[] arguments ) {
