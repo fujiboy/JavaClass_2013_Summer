11a8c70b9fb58debce0191dc2cf0dfcddcd48dcf
Brian Craig <bcraig108@gmail.com>
2013-05-31 16:31:40 -0400

    Initial commit to github
    Uploaded lesson 1 examples and PPT slides

---
+++ Examples/src/HelloWorld.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Brian & Denise
+ */
+public class HelloWorld {
+    public static void main( String[] arguments ) {
+        String greeting = "Hello World!";
+        System.out.println( greeting );
+    }
+}
