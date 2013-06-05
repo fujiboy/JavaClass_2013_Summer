21b682b5fa55e801365718fbea07fed9f3b792e5
Zachary Craig <bcraig108@verizon.net>
2013-06-04 23:15:06 -0400

    Remove homework

--- Homework_Zachary/src/NewRoot.java
+++
@@ -1,8 +0,0 @@
-public class NewRoot
-{
-    public static void main(String [] arguments)
-    {
-        int number = 625;
-        System.out.println("The square root of " + number + " is " +Math.sqrt(number));
-    }
-}
