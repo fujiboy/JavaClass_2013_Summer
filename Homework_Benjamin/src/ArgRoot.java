32bbb90bc277f499aac74b640265015708bae1b4
Benjamin Craig <bcraig108@verizon.net>
2013-06-08 12:41:42 -0400

    finished homework 1

---
+++ Homework_Benjamin/src/ArgRoot.java
@@ -0,0 +1,10 @@
+public class ArgRoot 
+{
+    public static void main (String[] arguments)
+    {
+    int number = Integer.decode (arguments [0]);
+    System.out.println(" The Square Root Of " + arguments [0] + " Is " + Math.sqrt(number));
+            
+    }
+}
+
---
+++ Homework_Benjamin/src/NewRoot.java
@@ -0,0 +1,9 @@
+public class NewRoot
+{
+    public static void main (String[] arguments)
+    {
+    int number = 625;
+    System.out.println( " The square root of " + number + " is " + Math.sqrt (number));
+
+}
+}
