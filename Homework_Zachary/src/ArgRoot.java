93063dc81b24f83ad3e850c9094366725e56ee26
Zachary Craig <bcraig108@verizon.net>
2013-06-10 17:57:04 -0400

    Zachary Homework 1

---
+++ Homework_Zachary/src/ArgRoot.java
@@ -0,0 +1,7 @@
+public class ArgRoot
+{
+    public static void main(String [] arguments)
+    {int number = Integer.decode (arguments[0]);
+    System.out.println("The square root of " + number + " is " +Math.sqrt(number));
+    }
+}
