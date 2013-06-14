428d649c2cb745437b6e7627f6b968401d67317b
Benjamin Craig <bcraig108@verizon.net>
2013-06-14 18:04:21 -0400

    finished homework 2

---
+++ Homework_Benjamin/src/PlanetWeight.java
@@ -0,0 +1,102 @@
+public class PlanetWeight
+{
+    public static void main (String [] arguments)
+    {
+        float number = Integer.decode (arguments [0]);
+        System.out.println ( " Hello, I'm going to show you your weights on other planets " );
+        System.out.println ( " Your weight on Earth is " + number );
+        System.out.println ( " Your weight on Mercury is " + number * 0.378);
+        System.out.println ( " Your weight on the moon is " + number * 0.166);
+        System.out.println ( " Your weight on Jupiter is " + number * 2.364);
+        System.out.println ( " Your weight on Venus is " + number * 0.907);
+        System.out.println ( " Your weight on Uranus is " + number * 0.889);
+        System.out.println ( " Your weight on the Sun is " + number * 27.022);
+        System.out.println ( " Your weight on the Mars is " + number * 0.38) ;
+        System.out.println ( " Your weight on the Saturn is " + number * 1.064) ;
+        System.out.println ( " Your weight on the Neptune is " + number * 1.125) ;
+        System.out.println ( " Your weight on the Pluto is " + number * 0.067) ;   
+        System.out.println ( " Thanks for testing me out today ");   
+    }
+     
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
